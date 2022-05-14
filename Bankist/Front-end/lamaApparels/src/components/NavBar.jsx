1badd18157a6d7bdd447f4e49c4c95f38e66943e
kelcheone <kelcheone@gmail.com>
2022-05-14 17:59:15 +0300

    feat: ⚛ landingpage

--- Bankist/Front-end/lamaApparels/src/components/NavBar.jsx
+++ Bankist/Front-end/lamaApparels/src/components/NavBar.jsx
@@ -1,16 +1,22 @@
 import styled from "styled-components";
-
+import SearchIcon from "@mui/icons-material/Search";
+import ShoppingCartOutlinedIcon from "@mui/icons-material/ShoppingCartOutlined";
+import Badge from "@mui/material/Badge";
 const Container = styled.div`
-  height: 100px;
+  height: 60px;
 `;
 const Wrapper = styled.div`
   padding: 10px 20px;
   display: flex;
   justify-content: space-between;
   flex: 1;
+  align-items: center;
 `;
 const Left = styled.div`
   flex: 1;
+  display: flex;
+  /* justify-content: flex-start; */
+  align-items: center;
 `;
 const Language = styled.span`
   font-size: 14px;
@@ -18,13 +24,36 @@ const Language = styled.span`
 `;
 
 const SearchContainer = styled.div`
-  border: 1px solid #ccc;
+  border: 0.5px solid lightgray;
+  display: flex;
+  align-items: center;
+  margin-left: 25px;
+  padding: 5px;
+`;
+
+const Input = styled.input`
+  border: none;
+`;
+
+const Logo = styled.div`
+  /* font-size: 30px; */
+  font-weight: bold;
+  text-align: center;
 `;
 const Center = styled.div`
   flex: 1;
 `;
 const Right = styled.div`
   flex: 1;
+  display: flex;
+  align-items: center;
+  justify-content: flex-end;
+`;
+
+const MenuItem = styled.div`
+  font-size: 14px;
+  cursor: pointer;
+  margin-left: 24px;
 `;
 const NavBar = () => {
   return (
@@ -33,12 +62,22 @@ const NavBar = () => {
         <Left>
           <Language>EN</Language>
           <SearchContainer>
-            input
-            <SearchIcon />
+            <Input />
+            <SearchIcon style={{ color: "gray", fontSize: 16 }} />
           </SearchContainer>
         </Left>
-        <Center>Center</Center>
-        <Right>Right</Right>
+        <Center>
+          <Logo>DRIPSTORE</Logo>
+        </Center>
+        <Right>
+          <MenuItem>Register</MenuItem>
+          <MenuItem>Sign In</MenuItem>
+          <MenuItem>
+            <Badge badgeContent={4} color="primary">
+              <ShoppingCartOutlinedIcon />
+            </Badge>
+          </MenuItem>
+        </Right>
       </Wrapper>
     </Container>
   );
