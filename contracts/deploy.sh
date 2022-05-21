775bfc5d0ecbfc877497376358a91fa9ce14af18
kelcheone <kelcheone@gmail.com>
2022-05-21 21:41:10 +0300

    feat(!): Ids & addresses

--- contracts/deploy.sh
+++ contracts/deploy.sh
@@ -1,3 +1,5 @@
-npx hardhat run ./scripts/deploySettlerMumbai.js  --network mumbai
-npx hardhat run ./scripts/deployReceiverRinkeby.js  --network rinkeby
+#!/bin/sh
+
+# npx hardhat run ./scripts/deploySettlerMumbai.js  --network mumbai
+npx hardhat run ./scripts/deploySettler.js  --network rinkeby
 npx hardhat run ./scripts/deployReceiverFuji.js  --network fuji
