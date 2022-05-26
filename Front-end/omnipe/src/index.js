a9a68e175bc1abe04935df5453262bfa307ec812
kelcheone <kelcheone@gmail.com>
2022-05-27 01:26:54 +0300

    feat: 🕸 omnipe landing page

--- Front-end/omnipe/src/index.js
+++ Front-end/omnipe/src/index.js
@@ -1,1 +1,7 @@
 // Create React entry point for javascript
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App';
+
+
+ReactDOM.render(<App />, document.getElementById('root'));
